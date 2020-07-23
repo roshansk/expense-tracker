3d8ae4da46bb493cbcdcd109dbc32c99944e5e65
Roshan Kamble <roshansk16@gmail.com>
2020-07-24 00:56:18 +0530

    Restored Comps

---
+++ src/comps/Counter.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    
+    render() { 
+        return ( 
+            <div className='counter pb-2'>
+                <h5 className='text-dark pb-2'>Your Wallet <button className='fas fa-redo-alt float-right border border-0' id='btn-reset' data-toggle='tooltip' data-placement='top' title='Resets The Wallet & Clears all Transactions' onClick={this.props.resetWallet}></button></h5> 
+                <div className='balance-spent my-2 text-dark shadow-sm'>
+                        <h5 className='m-0'>Balance : <span className='text-success'>{this.props.balance}</span></h5>
+                        <h5 className='m-0'>Expense : <span className='text-danger'>{this.props.spent}</span></h5>   
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default Counter;
