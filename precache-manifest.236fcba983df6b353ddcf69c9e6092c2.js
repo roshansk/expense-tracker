6d607d0e36d861b22c4a1f062ccc1f6d3fff8a70
Roshan Kamble <roshansk16@gmail.com>
2020-07-24 16:00:21 +0530

    Updates

---
+++ precache-manifest.236fcba983df6b353ddcf69c9e6092c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e920bfec0cc442e65e40e1caa41e05",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "01267e01142d096eb138",
+    "url": "/expense-tracker/static/css/main.f0848350.chunk.css"
+  },
+  {
+    "revision": "32c355f11389b5273f16",
+    "url": "/expense-tracker/static/js/2.5c6c7b36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.5c6c7b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01267e01142d096eb138",
+    "url": "/expense-tracker/static/js/main.10ee4c40.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  },
+  {
+    "revision": "45acf882b5c680f24f6190464d02af90",
+    "url": "/expense-tracker/static/media/bitcoin.45acf882.svg"
+  }
+]);
