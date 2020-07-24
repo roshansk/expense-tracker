83f6867c534224ab7984cad3faa993131d5b2600
Roshan Kamble <roshansk16@gmail.com>
2020-07-24 15:57:17 +0530

    UI Changes

--- src/comps/Counter.js
+++ src/comps/Counter.js
@@ -5,8 +5,8 @@ class Counter extends Component {
     render() { 
         return ( 
             <div className='counter pb-2'>
-                <h5 className='text-dark pb-2'>Your Wallet <button className='fas fa-redo-alt float-right' id='btn-reset' data-toggle='tooltip' data-placement='top' title='Resets The Wallet & Clears all Transactions' onClick={this.props.resetWallet}></button></h5> 
-                <div className='balance-spent my-2 text-dark shadow-sm'>
+                <h5 className='pb-2'>Your <span className='accent'>Wallet</span><button className='btn float-right' id='btn-reset' data-toggle='tooltip' data-placement='top' title='Resets The Wallet & Clears all Transactions pb-4' onClick={this.props.resetWallet}><i className='fas fa-redo-alt'></i></button></h5> 
+                <div className='balance-spent my-2 shadow-sm'>
                         <h5 className='m-0'>Balance : <span className='text-success'>{this.props.balance}</span></h5>
                         <h5 className='m-0'>Expense : <span className='text-danger'>{this.props.spent}</span></h5>   
                 </div>
